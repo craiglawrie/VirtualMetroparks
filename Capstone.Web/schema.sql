ab622c4309c96179ffb6cea0ec02832565621cde
Tyler Grant <tygrant10@gmail.com>
2018-04-11 16:28:54 -0400

    Commiting sql file.

--- Capstone.Web/schema.sql
+++ Capstone.Web/schema.sql
@@ -1,8 +1,8 @@
-﻿CREATE DATABASE Project;
+﻿CREATE DATABASE ParkInfo;
 
 GO
 
-USE Project;
+USE ParkInfo;
 
 GO
 
@@ -18,9 +18,30 @@ CREATE TABLE [dbo].[Users]
 CREATE TABLE [dbo].[UserRoles]
 (
 	[UserId] UNIQUEIDENTIFIER NOT NULL,
-	[Role] VARCHAR(MAX) NOT NULL,
+	[Role] VARCHAR(100) NOT NULL,
 
 	CONSTRAINT pk_UserRoles PRIMARY KEY (UserId, Role),
 	CONSTRAINT fk_UserRoles_Users FOREIGN KEY (UserId) REFERENCES Users(UserId)
+);
+
+CREATE TABLE [Parks]
+(
+[ParkId] integer identity(1,1) NOT NULL, 
+[ParkName]  VARCHAR(MAX) NOT NULL, 
+[ParkDescription]  VARCHAR(MAX) NOT NULL,
+[ParkLat] FLOAT NOT NULL,
+[ParkLong] FLOAT NOT NULL,
+[defaultZoom] int NOT NULL,
+CONSTRAINT pk_ParkId PRIMARY KEY (ParkId)
+);
+
+CREATE TABLE [Trails]
+(
+[TrailId] integer identity NOT NULL,
+[ParkId] integer NOT NULL, 
+[TrailName]  VARCHAR(MAX) NOT NULL, 
+[TrailDescription]  VARCHAR(MAX) NOT NULL,
+CONSTRAINT pk_TrailId PRIMARY KEY (TrailId),
+CONSTRAINT fk_ParkId foreign key (ParkId) REFERENCES Parks(ParkId)
 );
  
