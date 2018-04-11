5f496387ef6bad1b21e3d5ab25c9be4db6585b27
Craig Lawrie <craig.i.lawrie@gmail.com>
2018-04-11 18:29:13 -0400

    add map to park page

--- Capstone.Web/Scripts/ParkMap.js
+++ Capstone.Web/Scripts/ParkMap.js
@@ -5,9 +5,6 @@ let domainAddress = 'http://localhost:55900';
 
 function initMap() {
     // location list
-    var southChagrin = { name: 'South Chagrin', lat: 41.4185, lng: -81.4252, link: 'http://www.google.com' };
-    var clevelandMetroparksZoo = { name: 'Cleveland Metroparks Zoo', lat: 41.4459, lng: -81.7126, link: 'http://www.google.com' };
-    var bradleyWoods = { name: 'Bradley Woods Reservation', lat: 41.412204, lng: -81.9562316, link: 'http://www.google.com' };
     var administrativeOffices = { name: 'Cleveland Metroparks Administrative Offices', lat: 41.4456, lng: -81.7178, link: 'http://www.google.com' };
 
     // map positioning
@@ -28,7 +25,6 @@ function initMap() {
                 parkDetails.lat = park.Latitude;
                 parkDetails.lng = park.Longitude;
                 parkDetails.link = domainAddress + `/VirtualTrails/ChooseTrail/${park.Name}`;
-                console.log(parkDetails.link);
 
                 marker[i] = new google.maps.Marker({
                     position: parkDetails,
@@ -44,52 +40,47 @@ function initMap() {
                 i++;
             });
         });
-
-    /*
-     
-    // manual markers
-    // South Chagrin
-    var southChagrinMarker = new google.maps.Marker({
-        position: southChagrin,
-        title: southChagrin.name,
-        map: map
-    });
-    google.maps.event.addListener(southChagrinMarker, 'click', function () {
-        window.location.href = southChagrin.link;
-    });
-
-    // Bradley Woods
-    var bradleyWoodsMarker = new google.maps.Marker({
-        position: bradleyWoods,
-        title: bradleyWoods.name,
-        map: map
-    });
-    google.maps.event.addListener(bradleyWoodsMarker, 'click', function () {
-        window.location.href = bradleyWoods.link;
-    });
-
-    // Zoo
-    var clevelandMetroparksZooMarker = new google.maps.Marker({
-        position: clevelandMetroparksZoo,
-        title: clevelandMetroparksZoo.name,
-        map: map
-    });
-    google.maps.event.addListener(clevelandMetroparksZooMarker, 'click', function () {
-        window.location.href = clevelandMetroparksZoo.link;
-    });
-
-    // Administrative Offices
-    var administrativeOfficesMarker = new google.maps.Marker({
-        position: administrativeOffices,
-        title: administrativeOffices.name,
-        map: map
-    });
-    google.maps.event.addListener(administrativeOfficesMarker, 'click', function () {
-        window.location.href = administrativeOffices.link;
-    });
-
-    */
 }
 
 
 
+function initParkMap() {
+    let parkName = window.location.pathname.substring(27);
+    fetch(domainAddress + `/api/park/${parkName}`)
+        .then(response => response.json())
+        .then(park => {
+            // location list
+            var parkCenter = { name: 'Park Center', lat: park.Latitude, lng: park.Longitude, link: 'http://www.google.com' };
+
+            // map positioning
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: park.Zoom,
+                center: parkCenter,
+            });
+
+            // markers from api
+            let marker = [];
+            let parkDetails = {};
+            park.Trails.forEach(function (trail) {
+                trailDetails.name = trail.Name;
+                trailDetails.lat = trail.Latitude;
+                trailDetails.lng = trail.Longitude;
+                trailDetails.link = domainAddress + `/VirtualTrails/ViewTrail/${trail.TrailHeadId}`;
+
+                marker[i] = new google.maps.Marker({
+                    position: trailDetails,
+                    title: trailDetails.name,
+                    map: map,
+                    destinationLink: trailDetails.link
+                });
+
+                google.maps.event.addListener(marker[i], 'click', function () {
+                    window.location.href = this.destinationLink;
+                })
+
+                i++;
+            });
+        });
+
+
+}
