ef08359a4f7239088899d3dd913964f83a490f94
Craig Lawrie <craig.i.lawrie@gmail.com>
2018-04-18 10:13:41 -0400

    loop background sounds, fix one panaramic link in db

--- Capstone.Web/Scripts/ParkMap.js
+++ Capstone.Web/Scripts/ParkMap.js
@@ -162,33 +162,40 @@ function MakeTour() {
             pannellum.viewer('panorama', viewerParameters);
 
             setBackgroundAudioForNewPanoramic(panoramicId);
-
-            //if (trail.TrailHead.BackgroundSoundClips.length > 0) {
-            //    let clipSelection = Math.floor(Math.random() * Math.floor(trail.TrailHead.BackgroundSoundClips.length));
-            //    audioFile = trail.TrailHead.BackgroundSoundClips[clipSelection].AudioAddress;
-            //    audio = new Audio(audioFile);
-            //    audio.play();
-            //}
         });
 }
+
 
 function setBackgroundAudioForNewPanoramic(destinationId) {
     fetch(domainAddress + `/api/panoramic/${destinationId}`)
         .then(response => response.json())
         .then(panoramic => {
-           
-            if (panoramic.BackgroundSoundClips.map(function (soundClip) { return soundClip.AudioAddress }).includes(audioFile)) {
-                console.log("HelloWorld");
-            } else {
-                if (panoramic.BackgroundSoundClips.length > 0) {
-                    let clipSelection = Math.floor(Math.random() * Math.floor(panoramic.BackgroundSoundClips.length));
-                    audioFile = panoramic.BackgroundSoundClips[clipSelection].AudioAddress;
-                    audio = new Audio(audioFile);
-                    audio.play();
-                }
+            let soundClips = panoramic.BackgroundSoundClips.map(function (soundClip) { return soundClip.AudioAddress });
+            if (!soundClips.includes(audioFile)) {
+                audioFile = getNewAudioFileFromArray(soundClips);
+                playAudio();
+            }
+            else {
+                audioFile = getNewAudioFileFromArray(soundClips);
             }
         });
+}
 
+function playAudio() {
+    audio = new Audio(audioFile);
+    audio.play();
+    audio.addEventListener("ended", function () {
+        playAudio();
+    });
+}
+
+function getNewAudioFileFromArray(soundClips) {
+    if (soundClips.length > 0) {
+        let clipSelection = Math.floor(Math.random() * Math.floor(soundClips.length));
+        audioFile = soundClips[clipSelection];
+    }
+    console.log(audioFile);
+    return audioFile;
 }
 
 function getParameterByName(name, url) {
