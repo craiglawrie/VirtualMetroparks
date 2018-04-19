4ccbf517339b21f1db5932b3fe408b1ce10188ff
Taniya Talukdar <taniyatalukdar@gmail.com>
2018-04-19 11:39:08 -0400

    Added App.config and updated parkSqlDAlTests and got them to pass

--- Capstone.Tests/ParkSqlDALTests.cs
+++ Capstone.Tests/ParkSqlDALTests.cs
@@ -5,23 +5,47 @@ using Capstone.Web.Models;
 using Capstone.Web.DAL;
 using System.Transactions;
 using System.Data.SqlClient;
+using System.Configuration;
+using System.Linq;
 
 namespace Capstone.Tests
 {
     [TestClass]
     public class ParkSqlDALTests
     {
-        public static string connection = @"Server=.\SqlExpress;Database=ParkInfo;Trusted_Connection=true";
+        static string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        ParkModel park;
+        ParkImagesModel parkImage;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            park = new ParkModel()
+            {
+                Name = "testPark",
+                Description = "testDescription",
+                Latitude = 41,
+                Longitude = 100,
+                Zoom = 200
+            };
+
+            parkImage = new ParkImagesModel()
+            {
+                Bit = true,
+                ImageAddress = "address"
+            };
+        }
 
         [TestMethod]
         public void GetAllParks()
         {
             using (TransactionScope transaction = new TransactionScope())
             {
-                ParkSqlDALTests.InsertFakePark(0, "test", "Beautiful", "image", 40, 80, 100);
-                ParkSqlDAL testClass = new ParkSqlDAL(connection);
-                List<ParkModel> park = testClass.GetAllParks();
-                Assert.AreEqual(19, park.Count);
+                int newParkId = ParkSqlDALTests.InsertFakePark(park);
+                ParkSqlDAL testClass = new ParkSqlDAL(connectionString);
+                List<ParkModel> parks = testClass.GetAllParks();
+                Assert.IsTrue(parks.Select(park => park.ParkId).Contains(newParkId));
             }
         }
 
@@ -30,10 +54,10 @@ namespace Capstone.Tests
         {
             using (TransactionScope transaction = new TransactionScope())
             {
-                ParkSqlDALTests.InsertFakePark(19, "test", "Beautiful", "image", 40, 80, 100);
-                ParkSqlDAL testClass = new ParkSqlDAL(connection);
-                ParkModel park = testClass.GetParkById(19);
-                Assert.AreEqual(19, park.ParkId);
+                int newParkId = ParkSqlDALTests.InsertFakePark(park);
+                ParkSqlDAL testClass = new ParkSqlDAL(connectionString);
+                ParkModel newPark = testClass.GetParkById(newParkId);
+                Assert.AreEqual(newPark.ParkId, newParkId);
             }
         }
 
@@ -42,30 +66,79 @@ namespace Capstone.Tests
         {
             using (TransactionScope transaction = new TransactionScope())
             {
-                ParkSqlDALTests.InsertFakePark(19, "test", "Beautiful", "image", 40, 80, 100);
-                ParkSqlDAL testClass = new ParkSqlDAL(connection);
-                ParkModel park = testClass.GetParkByParkName("test");
-                Assert.AreEqual("test", park.Name);
+                int newParkId = ParkSqlDALTests.InsertFakePark(park);
+                ParkSqlDAL testClass = new ParkSqlDAL(connectionString);
+                ParkModel newPark = testClass.GetParkByParkName(park.Name);
+                Assert.AreEqual(newPark.ParkId, newParkId);
             }
         }
 
-        public static int InsertFakePark(int parkId, string name, string description, string image, double latitude, double longitude, int zoom)
+        [TestMethod]
+        public void GetImageByParkId()
         {
-            using (SqlConnection conn = new SqlConnection(connection))
+            using (TransactionScope transaction = new TransactionScope())
+            {
+                int newParkId = ParkSqlDALTests.InsertFakePark(park);
+                int newParkImageId = ParkSqlDALTests.InsertFakeParkImage(parkImage, newParkId);
+                ParkSqlDAL testClass = new ParkSqlDAL(connectionString);
+                string newParkImageAddress = testClass.GetImageByParkId(newParkId);
+                Assert.AreEqual(parkImage.ImageAddress, newParkImageAddress);
+            }
+        }
+
+        public static int InsertFakePark(ParkModel park)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 conn.Open();
-                SqlCommand cmd = new SqlCommand("INSERT INTO parks VALUES (@name, @description, @image, @latitude, @longitude, @zoom)", conn);
-                cmd.Parameters.AddWithValue("@name", name);
-                cmd.Parameters.AddWithValue("@description", description);
-                cmd.Parameters.AddWithValue("@image", image);
-                cmd.Parameters.AddWithValue("@latitude", latitude);
-                cmd.Parameters.AddWithValue("@longitude", longitude);
+                SqlCommand cmd = new SqlCommand("INSERT INTO parks " +
+                                              " (park_name, park_description, park_latitude, park_longitude, default_zoom) " +
+                                              "VALUES " +
+                                              " (@name, @description, @latitude, @longitude, @zoom)", conn);
+                cmd.Parameters.AddWithValue("@name", park.Name);
+                cmd.Parameters.AddWithValue("@description", park.Description);
+                cmd.Parameters.AddWithValue("@latitude", park.Latitude);
+                cmd.Parameters.AddWithValue("@longitude", park.Longitude);
+                cmd.Parameters.AddWithValue("@zoom", park.Zoom);
+
 
                 cmd.ExecuteNonQuery();
 
-                cmd = new SqlCommand("SELECT MAX(park_id) FROM parks", conn);
+                cmd = new SqlCommand("SELECT park_id FROM parks " +
+                                     "WHERE parks.park_name = @name " +
+                                     "AND parks.park_description = @description " +
+                                     "AND parks.park_latitude = @latitude " +
+                                     "AND parks.park_longitude = @longitude " +
+                                     "AND parks.default_zoom = @zoom", conn);
+                cmd.Parameters.AddWithValue("@name", park.Name);
+                cmd.Parameters.AddWithValue("@description", park.Description);
+                cmd.Parameters.AddWithValue("@latitude", park.Latitude);
+                cmd.Parameters.AddWithValue("@longitude", park.Longitude);
+                cmd.Parameters.AddWithValue("@zoom", park.Zoom);
                 int result = Convert.ToInt32(cmd.ExecuteScalar());
                 return result;
+
+
+            }
+        }
+
+        public static int InsertFakeParkImage(ParkImagesModel parkImage, int parkId)
+        {
+            int parkImageId = -1;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO park_images " +
+                                              "(park_id, park_image_address, local) " +
+                                              "VALUES (@parkId, @imageAddress, @bit)", conn);
+                cmd.Parameters.AddWithValue("@parkId", parkId);
+                cmd.Parameters.AddWithValue("@imageAddress", parkImage.ImageAddress);
+                cmd.Parameters.AddWithValue("@bit", parkImage.Bit);
+
+
+                cmd.ExecuteNonQuery();
+
+                return parkImageId;
             }
         }
     }
