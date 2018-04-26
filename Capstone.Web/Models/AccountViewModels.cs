cc38a164376a6ee3c2fcab6f4a24e83434ea32e1
Bethany Warner <32970347+hp-plus5@users.noreply.github.com>
2018-04-26 13:45:56 -0400

    Centered login, register, and addhotspot forms

--- Capstone.Web/Models/AccountViewModels.cs
+++ Capstone.Web/Models/AccountViewModels.cs
@@ -25,7 +25,7 @@ namespace Capstone.Web.Models
         [Display(Name = "Password")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Remember me")]
         public bool RememberMe { get; set; }
     }
 
